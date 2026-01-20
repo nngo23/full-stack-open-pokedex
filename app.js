4ea72755ec9264184a4d5dd6e5f976ab6ed64d9c
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-20 15:55:14 +0200

    11.12 codes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
-app.get('/version', (req, res) => {
-  res.send('1')
-})
+app.get("/version", (req, res) => {
+  res.send("1");
+});
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
+
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw "error...  ";
+  res.send("ok");
+});
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
