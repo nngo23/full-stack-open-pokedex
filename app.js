2d18be0d2fef4dbf6aa992097a614af85bebf08d
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-20 15:21:05 +0200

    fix eslint

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
-app.get("/version", (req, res) => {
-  res.send("1");
-});
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
