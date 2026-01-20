8dd6c8d7a1e15710e5d3627b1f55bbdca1e721a1
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-20 18:07:30 +0200

    update port to make deployment failed

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-import express from 'express'
-const app = express()
+import express from "express";
+const app = express();
 
-app.get('/version', (req, res) => {
-  res.send('1')
-})
+app.use(express.static("dist"));
+
+app.get("/version", (req, res) => {
+  res.send("1");
+});
+
+app.get("/health", (req, res) => {
+  res.status(500).send("broken");
+});
 // get the port from env variable
-const PORT = process.env.PORT || 5000
-
-app.use(express.static('dist'))
-
-app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
-  res.send('ok')
-})
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  if (process.env.CRASH === "true") throw new Error("Crash for testing");
+  console.log(`server started on port ${PORT}`);
+});
