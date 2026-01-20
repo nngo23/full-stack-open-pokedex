1c5c70afe98a18564a4acbc4867d6c9a2df5ddf8
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2026-01-20 18:26:56 +0200

    fix error deployment simulation

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  res.status(500).send('broken')
+  res.send('ok')
 })
-// get the port from env variable
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  if (process.env.CRASH === 'true') throw new Error('Crash for testing')
   console.log(`server started on port ${PORT}`)
 })
